045256b6ba90fabb5a45cbe7d1b9d7435349bf5d
Onethoww <theaisim@gmail.com>
2025-01-25 21:54:49 -0600

    Add files via upload

---
+++ leaderboardData.js
@@ -0,0 +1,14 @@
+export const leaderboardData = {
+    "result": [
+        { "m": 83, "u": [{ "title": "God's child", "username": "robbensino" }], "p": "7.54", "a": 7 },
+        { "m": 72, "u": [{ "title": "Timi_Crypt😎", "username": "kayode_32" }], "p": "3.55", "a": 7 },
+        { "m": 62, "u": [{ "title": "Lekky", "username": "lekky_sol" }], "p": "6.79", "a": 7 },
+        { "m": 50, "u": [{ "title": "Tommy Light", "username": "t0mmy_7" }], "p": "4.60", "a": 6 },
+        { "m": 39, "u": [{ "title": "THE REAL NST🐈‍⬛", "username": "bolarinwanst" }], "p": "2.63", "a": 5 },
+        { "m": 32, "u": [{ "title": "ÃyØm_MãRvË", "username": "ayomide5683141" }], "p": "1.71", "a": 5 },
+        { "m": 20, "u": [{ "title": "Laycon🐾", "username": "laycon_web3" }], "p": "1.62", "a": 1 },
+        { "m": 18, "u": [{ "title": "AY", "username": "pen_gameo" }], "p": "2.03", "a": 5 },
+        { "m": 17, "u": [{ "title": "Highdee", "username": "highdee2105" }], "p": "2.01", "a": 2 },
+        { "m": 13, "u": [{ "title": "Alihaydar", "username": "a1ihydr" }], "p": "2.39", "a": 1 }
+    ]
+};
